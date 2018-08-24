7cb3933b59c0775dc2fec033f282b41bda9f15f1
Kevin <kieliszek52@gmail.com>
2018-08-24 15:43:24 -0500

    More work

---
+++ DatingApp.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using DatingApp.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DatingApp.API
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DataContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlite(connectionString);
+
+            return new DataContext(builder.Options);
+        }
+    }
+}
--- DatingApp.Repositories/ValuesRepository.cs
+++ DatingApp.Repositories/ValuesRepository.cs
@@ -2,10 +2,7 @@
 using DatingApp.Data.Models;
 using DatingApp.Interfaces.Repository;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DatingApp.Repositories
