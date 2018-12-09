84a73582cf0cabdef493efc87df1b70640059075
Kevin <kieliszek52@gmail.com>
2018-12-09 14:19:16 -0600

    Work

--- DatingApp.Data/Models/User.cs
+++ DatingApp.Data/Models/User.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace DatingApp.Data.Models
 {
@@ -10,5 +9,16 @@ namespace DatingApp.Data.Models
         public string Username { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public string gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor   { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
